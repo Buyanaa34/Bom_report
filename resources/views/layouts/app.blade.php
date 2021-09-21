dfb31579a47f0679bb343195610c4fb974a0b138
Buyankhishig <68805269+Buyanaa34@users.noreply.github.com>
2021-09-21 11:23:45 +0800

    9/21

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,12 @@
+<!doctype html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>BOM reports</title>
+    </head>
+    <body>
+        @yield('content')
+        <h1>this is app</h1>     
+    </body>
+</html>
