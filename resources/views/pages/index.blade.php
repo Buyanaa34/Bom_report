a01598c940e5698968630d30343e8d072d755fec
Buyankhishig <68805269+Buyanaa34@users.noreply.github.com>
2021-09-21 15:43:29 +0800

    9/21 Javascript

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -11,88 +11,65 @@
     <table id="example" class="display" style="width:100%">
         <thead>
             <tr>
-                <th>Name</th>
-                <th>Position</th>
-                <th>Office</th>
-                <th>Age</th>
-                <th>Start date</th>
-                <th>Salary</th>
+                {{-- GARCHIG---G JS-EER OLGOH --}}
+                <script>
+                    let array_1 = ["Acq","Time","Pan","Termclass","Trace","Approval","RRN","Amount","NETC fee","Iss fee","Settlement amount"]
+                    array_1.forEach(element => {
+                        document.write("<th>"+element+"</th>")
+                    });
+                </script>
             </tr>
         </thead>
-        <tbody>
-            <tr>
-                <td>Tiger Nixon</td>
-                <td>System Architect</td>
-                <td>Edinburgh</td>
-                <td>61</td>
-                <td>2011/04/25</td>
-                <td>$320,800</td>
+        <tbody style="text-align: center">
+            <tr >
+                <td>VALUE1</td>
+                <td>VALUE2</td>
+                <td>VALUE3</td>
+                <td>VALUE4</td>
+                <td>VALUE5</td>
+                <td>VALUE6</td>
+                <td>VALUE7</td>
+                <td>VALUE8</td>
+                <td>VALUE9</td>
+                <td>VALUE10</td>
+                <td>VALUE11</td>
+            </tr>
+            <tr >
+                <td>VAL1</td>
+                <td>VAL2</td>
+                <td>VALE3</td>
+                <td>VALUE4</td>
+                <td>VALUE5</td>
+                <td>VALUE6</td>
+                <td>VALUE7</td>
+                <td>VALUE8</td>
+                <td>VALUE9</td>
+                <td>VALUE10</td>
+                <td>VALUE11</td>
             </tr>
             <tr>
-                <td>Garrett Winters</td>
-                <td>Accountant</td>
-                <td>Tokyo</td>
-                <td>63</td>
-                <td>2011/07/25</td>
-                <td>$170,750</td>
+                <td>VALUE1</td>
+                <td>VALUE2</td>
+                <td>VALUE3</td>
+                <td>VALUE4</td>
+                <td>VALUE5</td>
+                <td>VALUE6</td>
+                <td>VALUE7</td>
+                <td>VALUE8</td>
+                <td>VALUE9</td>
+                <td>VALUE10</td>
+                <td>VALUE11</td>
             </tr>
-            <tr>
-                <td>Ashton Cox</td>
-                <td>Junior Technical Author</td>
-                <td>San Francisco</td>
-                <td>66</td>
-                <td>2009/01/12</td>
-                <td>$86,000</td>
-            </tr>
-            <tr>
-                <td>Cedric Kelly</td>
-                <td>Senior Javascript Developer</td>
-                <td>Edinburgh</td>
-                <td>22</td>
-                <td>2012/03/29</td>
-                <td>$433,060</td>
-            </tr>
-            <tr>
-                <td>Airi Satou</td>
-                <td>Accountant</td>
-                <td>Tokyo</td>
-                <td>33</td>
-                <td>2008/11/28</td>
-                <td>$162,700</td>
-            </tr>
-            <tr>
-                <td>Brielle Williamson</td>
-                <td>Integration Specialist</td>
-                <td>New York</td>
-                <td>61</td>
-                <td>2012/12/02</td>
-                <td>$372,000</td>
-            </tr>
-            <tr>
-                <td>Herrod Chandler</td>
-                <td>Sales Assistant</td>
-                <td>San Francisco</td>
-                <td>59</td>
-                <td>2012/08/06</td>
-                <td>$137,500</td>
-            </tr>
-            <tr>
-                <td>Rhona Davidson</td>
-                <td>Integration Specialist</td>
-                <td>Tokyo</td>
-                <td>55</td>
-                <td>2010/10/14</td>
-                <td>$327,900</td>
-            </tr>            
         </tbody>
         <tfoot>
             <tr>
-                <th>Name</th>
-                <th>Position</th>
-                <th>Office</th>
-                <th>Age</th>
-                <th>Start date</th>
-                <th>Salary</th>
+                {{-- GARCHIG---G JS-EER OLGOH --}}
+                <script>
+                    let array = ["Acq","Time","Pan","Termclass","Trace","Approval","RRN","Amount","NETC fee","Iss fee","Settlement amount"]
+                    array.forEach(element => {
+                        document.write("<th>"+element+"</th>")
+                    });
+                </script>
             </tr>
         </tfoot>
     </table>
